0269c22cd605328aab397e9538b6bd87dddf47a2
Vinícius Inácio <sobral.321@hotmail.com>
2025-11-08 18:03:55 -0300

    chore: app colors

---
+++ lib/ui/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// This class is used to store all the custom colors for the app.
+class MyAppColors {
+  static const primary = Color(0xFF1876D2);
+  static const white = Color(0xFFFFFFFF);
+  static const secondary = Color(0xFF0D478C);
+  static const black = Color(0xFF0E0D0D);
+  static const black2 = Color(0xFF0B1125);
+  static const greyInputBorder = Color(0xFFBFBFBF);
+  static const redErrorColor = Color(0xFFF5222D);
+  static const greenSuccesColor = Color(0xFF81DC96);
+  static const greyLabel = Color(0xFF666666);
+}
